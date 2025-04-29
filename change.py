93dad076a8bc6bb139214e13db96185d763a8196
Aayush08102013 <aayush.ranjan0815@gmail.com>
2025-04-29 16:42:26 +1200

    Add files via upload

---
+++ change.py
@@ -0,0 +1,6 @@
+bill_totoal = float(input("enter your amount: "))
+money_paid = float(input("enter your  paid amount: "))
+
+change1 = bill_totoal - money_paid
+
+print(f"youre change money is {change1} ")
